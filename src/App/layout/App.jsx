f6df94c08bbbcb087494d1d0e9a580a9fd56287e
Azam Hussain <azamhussain.53@gmail.com>
2018-08-18 14:46:47 +0530

    added event form

--- src/App/layout/App.jsx
+++ src/App/layout/App.jsx
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
-
+import EventDashboard from '../../Features/event/EventDashboard/EventDashboard'
+import NavBar from '../../Features/NavBar/NavBar'
+import { Container } from 'semantic-ui-react'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>React App</h1>
-        
+      <div>
+        <NavBar />
+        <Container className="main">
+          <EventDashboard />
+        </Container>
       </div>
     );
   }
